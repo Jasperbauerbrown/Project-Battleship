0734280d02593df8001994cdbb88c2a50ec70b16
ingjacob <83359834+ingjacob@users.noreply.github.com>
2021-05-10 08:40:55 -0600

    Update Opponent.cpp

--- Project Battleship/Opponent.cpp
+++ Project Battleship/Opponent.cpp
@@ -2,7 +2,7 @@
 
 Opponent::Opponent();
 Opponent::~Opponent();
-int runGuess(int guessNum)
+void runGuess(int guessNum)
 {
     srand((unsigned)time(0));
     do
@@ -10,6 +10,14 @@ int runGuess(int guessNum)
         tryAgain = false;
         xGuess = ((rand() % 4) + 1);
         yGuess = ((rand() % 4) + 1);
-        if (guessx[guessNum] == xGuess && guessy[guessNum] == yGuess) tryAgain = true;
+        if (guessNum > 0)
+        {
+            for (int i = guessNum; i >= 0; i--)
+            {
+                if (guessx[i] == xGuess && guessy[i] == yGuess) tryAgain = true;
+            }
+        }
     }while(tryAgain = true)
+    guessx[guessNum] = xGuess;
+    guessy[guessNum] = yGuess;
 }
