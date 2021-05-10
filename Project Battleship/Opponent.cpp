5a988139a39c8c4230f64749b6c131e3aa17eedd
ingjacob <83359834+ingjacob@users.noreply.github.com>
2021-05-10 08:33:38 -0600

    Update Opponent.cpp

--- Project Battleship/Opponent.cpp
+++ Project Battleship/Opponent.cpp
@@ -1,1 +1,15 @@
 #include "Opponent.h"
+
+Opponent::Opponent();
+Opponent::~Opponent();
+int runGuess(int guessNum)
+{
+    srand((unsigned)time(0));
+    do
+    {
+        tryAgain = false;
+        xGuess = ((rand() % 4) + 1);
+        yGuess = ((rand() % 4) + 1);
+        if (guessx[guessNum] == xGuess && guessy[guessNum] == yGuess) tryAgain = true;
+    }while(tryAgain = true)
+}
