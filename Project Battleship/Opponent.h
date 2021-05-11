25b75fcac0a6ed7fe8320db6de551e682f911f31
ingjacob <83359834+ingjacob@users.noreply.github.com>
2021-05-10 22:59:03 -0600

    Update Opponent.h

--- Project Battleship/Opponent.h
+++ Project Battleship/Opponent.h
@@ -7,15 +7,17 @@
 using namespace std;
 class opponent {
 public:
-	opponent();
-	~opponent();
-	int shipsx[5];
-	int shipsy[5];
-	vector <int> guessx;
-	vector <int> guessy;
-	void runGuess(int);
-	int xGuess;
-	int yGuess;
-	bool tryAgain;
+    opponent();
+    ~opponent();
+    int shipsx[5];
+    int shipsy[5];
+    vector <int> guessx;
+    vector <int> guessy;
+    void runGuess(int);
+    int returnGuessx(int);
+    int returnGuessy(int);
+    int xGuess;
+    int yGuess;
+    bool tryAgain;
 };
 #endif
